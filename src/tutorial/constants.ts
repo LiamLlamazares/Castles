7235d6c19fd3a8d87b74f140916281adfa15f435
Liam Llamazares <s2266126@ed.ac.uk>
2026-01-01 00:29:44 +0100

    Update tutorial

--- src/tutorial/constants.ts
+++ src/tutorial/constants.ts
@@ -9,15 +9,15 @@ import { getStartingLayout } from '../ConstantImports';
 export const TUTORIAL_BOARD_MINI_R = 2;      // 7-hex cluster
 export const TUTORIAL_BOARD_SMALL_R = 3;     // Small scenarios
 export const TUTORIAL_BOARD_MEDIUM_R = 4;    // Medium scenarios
-export const TUTORIAL_BOARD_STANDARD_R = 6;  // Full game
+export const TUTORIAL_BOARD_STANDARD_R = 5;  // Full game
 // Reusable castle configurations
 export const TUTORIAL_CASTLES_STANDARD = [
-  new Castle(new Hex(-4, 2, 2), 'w', 0),
-  new Castle(new Hex(4, -2, -2), 'b', 0),
-  new Castle(new Hex(-1,2, -1), 'w', 0),
-  new Castle(new Hex(-1, -2, 3), 'b', 0),
-  new Castle(new Hex(3, 2, -5), 'w', 0),
-  new Castle(new Hex(-3, -2, 5), 'b', 0),
+  new Castle(new Hex(0, 4, -4), 'w', 0),
+  new Castle(new Hex(0, -4, 4), 'b', 0),
+  new Castle(new Hex(-2,4, -2), 'w', 0),
+  new Castle(new Hex(2, -4, 2), 'b', 0),
+  new Castle(new Hex(-4, 1, 3), 'w', 0),
+  new Castle(new Hex(4, -1, -3), 'b', 0),
 ] as const;
 export const TUTORIAL_CASTLES_SMALL = [
   new Castle(new Hex(-3, 3, 0), 'w', 0),
@@ -29,7 +29,8 @@ export const TUTORIAL_CASTLES_MINI = [
 ] as const;
 
 export const TUTORIAL_SANCTUARIES_STANDARD = [
-  new Sanctuary(new Hex(0, 0, 0), SanctuaryType.WolfCovenant, 'w', null, 0, false),
-  new Sanctuary(new Hex(-2, -2, 4), SanctuaryType.ArcaneRefuge, 'b', null, 0, false),  // Wizard
-  new Sanctuary(new Hex(2, 2, -4), SanctuaryType.PyreEternal, 'w', null, 0, false),   // Phoenix
+  new Sanctuary(new Hex(4, 1, -5), SanctuaryType.WolfCovenant, 'w', null, 0, false),
+  new Sanctuary(new Hex(-4, -1, 5), SanctuaryType.ArcaneRefuge, 'b', null, 0, false),  // Wizard
+  // new Sanctuary(new Hex(-5, 4, 1), SanctuaryType.PyreEternal, 'w', null, 0, false),   // Phoenix
+  // new Sanctuary(new Hex(5, -4, -1), SanctuaryType.PyreEternal, 'b', null, 0, false),   // Phoenix
 ] as const;
